06423ddbc4011b71ac0fe2b9bbb9a0e89df1ec87
tusharthakur09 <thakurtushar1309@gmail.com>
2024-12-23 15:02:02 +0530

    cors

--- Backend/index.js
+++ Backend/index.js
@@ -1,36 +1,35 @@
-import express, { urlencoded } from 'express';
-import cookieParser from 'cookie-parser';
-import cors from 'cors';
-import dotenv from 'dotenv';
-import connectDB from './utils/db.js';
-import userRoute from '../Backend/Routes/user.route.js';
-import companyRoute from '../Backend/Routes/company.route.js';
-import jobRoute from '../Backend/Routes/job.route.js';
-import applicationRoute from '../Backend/Routes/application.route.js'
+import express, { urlencoded } from "express";
+import cookieParser from "cookie-parser";
+import cors from "cors";
+import dotenv from "dotenv";
+import connectDB from "./utils/db.js";
+import userRoute from "../Backend/Routes/user.route.js";
+import companyRoute from "../Backend/Routes/company.route.js";
+import jobRoute from "../Backend/Routes/job.route.js";
+import applicationRoute from "../Backend/Routes/application.route.js";
 dotenv.config();
 const app = express();
 connectDB();
 
 //middlewares
 app.use(express.json());
-app.use(express.urlencoded({extended:true}));
+app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 const corsOptions = {
-    origin:'http://localhost:5173',
-    credentials:true
-}
+  origin: "http://localhost:5173",
+  credentials: true,
+};
 
-app.use(cors(corsOptions))
+app.use(cors());
 
 // const PORT = 3000;
 
-
 //api's
-app.use("/api/v1/user",userRoute);
-app.use("/api/v1/company",companyRoute);
-app.use("/api/v1/job",jobRoute);
-app.use("/api/v1/Application",applicationRoute);
+app.use("/api/v1/user", userRoute);
+app.use("/api/v1/company", companyRoute);
+app.use("/api/v1/job", jobRoute);
+app.use("/api/v1/Application", applicationRoute);
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-    console.log(`Server running at port ${PORT}`);
+  console.log(`Server running at port ${PORT}`);
 });
